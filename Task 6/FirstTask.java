93b694b67417c5afc4bfdf6753fa957862d165bc
Veka Dzmitry <vekodmitriy0@gmail.com>
2019-11-22 08:48:40 +0300

    feat: implement PlaneModule class

--- Task 6/FirstTask.java
+++ Task 6/FirstTask.java
@@ -5,4 +5,45 @@ public class FirstTask {
     public static void main(String[] args) {
         
     }
+
+    /**
+    * PlaneModule
+    */
+    public class PlaneModule {
+        String condition;
+        boolean isReady;
+
+        PlaneModule() {}
+
+        PlaneModule(String condition, boolean isReady) {
+            this.condition = condition;
+            this.isReady = isReady;
+        }
+
+        void setReadiness(boolean value) {
+            this.isReady = value;
+        }
+
+        boolean getReadiness() {
+            return this.isReady;
+        }
+
+        void setCondition(String value) {
+            if (value.length() < 30) {
+                this.condition = value;
+            }
+        }
+
+        String getCondition() {
+            if (this.condition.length() == 0) {
+                return "Condition is empty";
+            }
+            else {
+                return this.condition;
+            }
+        }
+    }
+
+   
 }
+
