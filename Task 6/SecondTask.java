a46edd3be9e8c6abb7f05c192aa5f3e8b90d27d9
Veka Dzmitry <vekodmitriy0@gmail.com>
2019-11-29 03:05:23 +0300

    Task6/feat: add class Flat

--- Task 6/SecondTask.java
+++ Task 6/SecondTask.java
@@ -1,29 +1,170 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+
 /**
  * SecondTask
  */
 public class SecondTask {
 
     public static void main(String[] args) {
-        homeElectricalApp microVawe = new homeElectricalApp();
-        microVawe.setPriority(1);
-        System.out.println(microVawe.getPriority());
+        HomeElectricalApp microWave = new HomeElectricalApp(2, 300, "microWave");
+        HomeElectricalApp tV = new HomeElectricalApp(4, 400, "tV");
+        HomeElectricalApp computer = new HomeElectricalApp(1, 800, "computer");
+        computer.turnOn();
+        tV.turnOn();
+        Flat myFlat = new Flat();
+        myFlat.addApp(computer);
+        myFlat.addApp(microWave);
+        myFlat.addApp(tV);
+        System.out.println(myFlat.countConsumePower());
+        myFlat.showAllApps();
+        myFlat.sortAppsByPower();
+        myFlat.showAllApps();
+        System.out.println(myFlat.findApp("computer").getName());
+        myFlat.turnOnApp("microWave");
+        System.out.println(myFlat.countConsumePower());
+        myFlat.turnOffAll();
+        System.out.println(myFlat.countConsumePower());
+        myFlat.delApp("tV");
+        myFlat.showAllApps();
     }
 }
 
 /**
- * electricalApp
+ * Flat
  */
-class homeElectricalApp {
+class Flat {
+    private ArrayList<HomeElectricalApp> flatElectricalApps;
+
+    Flat() {
+        this.flatElectricalApps = new ArrayList<HomeElectricalApp>();
+    }
+
+    Flat(HomeElectricalApp apps[]) {
+        this.flatElectricalApps = new ArrayList<HomeElectricalApp>();
+        for (int i = 0; i < apps.length; i++) {
+            this.flatElectricalApps.add(apps[i]);
+        }
+    }
+
+    void addApp(HomeElectricalApp app) {
+        this.flatElectricalApps.add(app);
+    }
+
+    void delApp(String name) {
+        for (int i = 0; i < this.flatElectricalApps.size(); i++) {
+            HomeElectricalApp currentApp = this.flatElectricalApps.get(i);
+            if(currentApp.getName() == name) {
+                this.flatElectricalApps.remove(i);
+                break;
+            }
+        }
+    }
+
+    int countConsumePower() {
+        int powerSum = 0;
+        for (int i = 0; i < this.flatElectricalApps.size(); i++) {
+            HomeElectricalApp currentApp = this.flatElectricalApps.get(i);
+            if(currentApp.isOn()) powerSum += currentApp.getCapacity();
+        }
+        return powerSum;
+    }
+
+    void sortAppsByPower() {
+        Comparator<HomeElectricalApp> comparator = new Comparator<HomeElectricalApp>() {
+            public int compare(HomeElectricalApp app1, HomeElectricalApp app2) {
+                int x = app1.getCapacity();
+                int y = app2.getCapacity();
+                return (x < y) ? -1 : ((x == y) ? 0 : 1);
+            }
+        };
+        this.flatElectricalApps.sort(comparator);
+    }
+
+    void turnOnApp(String name) {
+        HomeElectricalApp app = this.findApp(name);
+        if (app != null) {
+            app.turnOn();
+        }
+    }
+
+    void turnOffApp(String name) {
+        HomeElectricalApp app = this.findApp(name);
+        if (app != null) {
+            app.turnOff();
+        }
+    }
+
+    void turnOnAll() {
+        for (int i = 0; i < this.flatElectricalApps.size(); i++) {
+            HomeElectricalApp currentApp = this.flatElectricalApps.get(i);
+            currentApp.turnOn();
+        }
+    }
+
+    void turnOffAll() {
+        for (int i = 0; i < this.flatElectricalApps.size(); i++) {
+            HomeElectricalApp currentApp = this.flatElectricalApps.get(i);
+            currentApp.turnOff();
+        }
+    }
+
+    HomeElectricalApp findApp(String name) {
+        for (int i = 0; i < this.flatElectricalApps.size(); i++) {
+            HomeElectricalApp currentApp = this.flatElectricalApps.get(i);
+            if(currentApp.getName() == name) return currentApp;
+        }
+        return null;
+    }
+
+    HomeElectricalApp findApp(String name, int priority) {
+        for (int i = 0; i < this.flatElectricalApps.size(); i++) {
+            HomeElectricalApp currentApp = this.flatElectricalApps.get(i);
+            if(currentApp.getName() == name && currentApp.getPriority() == priority) return currentApp;
+        }
+        return null;
+    }
+
+    HomeElectricalApp findApp(String name, int priority, int capacity) {
+        for (int i = 0; i < this.flatElectricalApps.size(); i++) {
+            HomeElectricalApp currentApp = this.flatElectricalApps.get(i);
+            if (
+                currentApp.getName() == name && 
+                currentApp.getPriority() == priority && 
+                currentApp.getCapacity() == capacity
+            ) 
+            return currentApp;
+        }
+        return null;
+    }
+
+    void showAllApps() {
+        for (int i = 0; i < this.flatElectricalApps.size(); i++) {
+            HomeElectricalApp currentApp = this.flatElectricalApps.get(i);
+            System.out.println(
+                "\t" + (i+1) + ". " + currentApp.getName() + 
+                ": Power - " + currentApp.getCapacity() + 
+                ": Priority - " + currentApp.getPriority()
+            );   
+        }
+        System.out.println();
+    }
+}
+
+/**
+ * HomeElectricalApp
+ */
+class HomeElectricalApp {
     private int priority;
     private int capacity;
     private String name;
     private boolean on;
 
-    homeElectricalApp() {
+    HomeElectricalApp() {
 
     }
 
-    homeElectricalApp(int priority, int capacity, String name) {
+    HomeElectricalApp(int priority, int capacity, String name) {
         this.priority = priority;
         this.capacity = capacity;
         this.name = name;
