4915c72e5118062427c375901bc0165721a7faf4
Veka Dzmitry <vekodmitriy0@gmail.com>
2019-12-04 16:03:25 +0300

    feat: implement task

--- Task 7/firstTask.java
+++ Task 7/firstTask.java
@@ -6,7 +6,34 @@ import java.util.Scanner;
 public class firstTask {
 
     public static void main(String[] args) {
-        
+        Gun myGun = new Gun(5);
+        System.out.println("ammo - " + myGun.ammo);
+        myGun.fire();
+        System.out.println("ammo - " + myGun.ammo);
+        Rifle myRifle = new Rifle(5, "mosinka");
+        myRifle.ammo = 1;
+        myRifle.fire();
+        myRifle.fire();
+        myRifle.ammo = 1;
+        myRifle.fire();
+        myRifle.fire();
+        System.out.println("ammo - " + myRifle.ammo);
+        myRifle.reload();
+        myRifle.fire();
+        System.out.println("ammo - " + myRifle.ammo);
+        System.out.println("ammo - " + myRifle.ammo);
+        AssaultRifle myAssaultRifle = new AssaultRifle(30, "stg 44", "single");
+        System.out.println("ammo - " + myAssaultRifle.ammo);
+        myAssaultRifle.fire();
+        System.out.println("ammo - " + myAssaultRifle.ammo);
+        myAssaultRifle.fireMode = "bursting";
+        myAssaultRifle.fire();
+        System.out.println("ammo - " + myAssaultRifle.ammo);
+        myAssaultRifle.reload(45);
+        System.out.println("ammo - " + myAssaultRifle.ammo);
+        myAssaultRifle.fireMode = "automatic";
+        myAssaultRifle.fire();
+        System.out.println("ammo - " + myAssaultRifle.ammo);
     }
 }
 
@@ -43,7 +70,12 @@ class Gun {
             this.ammo--;
         }
         else {
-            System.out.println("Reload Gun!!!");
+            System.out.println("Reload Gun!!!(input \'1\')");
+            Scanner in = new Scanner(System.in);
+            if (in.nextInt() == 1) {
+                this.reload();
+            }
+            System.out.println("Gun Ready!!!");
         }
     }
 }
@@ -80,9 +112,22 @@ class AssaultRifle extends Rifle {
         System.out.println("\t 1 - single");
         System.out.println("\t 2 - bursting");
         System.out.println("\t 3 - automatic");
-        Scanner in = new Scanner(System.in);
-        this.fireMode = in.nextLine();
-        in.close();
+        Scanner in1 = new Scanner(System.in);
+        int txt = in1.nextInt();
+        switch (txt) {
+            case 1: {
+                this.fireMode = "single";
+                break;
+            }
+            case 2: {
+                this.fireMode = "bursting";
+                break;
+            }
+            case 3: {
+                this.fireMode = "automatic";
+                break;
+            }
+        }
         System.out.println("\t switched to" + this.fireMode + "fire mode");
     }
     
@@ -97,7 +142,12 @@ class AssaultRifle extends Rifle {
                     this.ammo -= 3;
                 }
                 else {
-                    System.out.println("Reload Gun!!!");
+                    System.out.println("Reload Gun!!!(input \'1\')");
+                    Scanner in = new Scanner(System.in);
+                    if (in.nextInt() == 1) {
+                        this.reload();
+                    }
+                    System.out.println("Gun Ready!!!");
                 }
                 break;
             }
@@ -105,11 +155,23 @@ class AssaultRifle extends Rifle {
                 if (!isClipEmpty()) {
                     while (!isClipEmpty()) {
                         this.ammo--;
+                        System.out.println("Shoot!");
                     }
                 }
                 else {
-                    System.out.println("Reload Gun!!!");
+                    System.out.println("Reload Gun!!!(input \'1\')");
+                    Scanner in = new Scanner(System.in);
+                    if (in.nextInt() == 1) {
+                        this.reload();
+                    } 
+                    System.out.println("Gun Ready!!!");
                 }
+                System.out.println("Reload Gun!!!(input \'1\')");
+                Scanner in = new Scanner(System.in);
+                if (in.nextInt() == 1) {
+                    this.reload();
+                } 
+                System.out.println("Gun Ready!!!");
                 break;
             }
         }
