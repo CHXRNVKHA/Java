fdb16785f450bd44ff5a10272ae11f2bdd3f87ff
Veka Dzmitry <vekodmitriy0@gmail.com>
2019-12-04 15:11:57 +0300

    feat: implement AssaultRifle class

--- Task 7/firstTask.java
+++ Task 7/firstTask.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * firstTask
  */
@@ -55,5 +57,61 @@ class Rifle extends Gun {
     Rifle(int clipCapacity, String name) {
         super(clipCapacity);
         this.name = name;
+    }
+}
+
+/**
+ * AssaultRifle
+ */
+class AssaultRifle extends Rifle {
+    String fireMode;
+    AssaultRifle(int clipCapacity, String name, String fireMode) {
+        super(clipCapacity, name);
+        this.fireMode = fireMode;
+    }
+
+    void reload (int clipCapacity) {
+        this.clipCapacity = clipCapacity;
+        this.ammo = clipCapacity;
+    }
+
+    void changeFireMode() {
+        System.out.println("choose fire mode");
+        System.out.println("\t 1 - single");
+        System.out.println("\t 2 - bursting");
+        System.out.println("\t 3 - automatic");
+        Scanner in = new Scanner(System.in);
+        this.fireMode = in.nextLine();
+        in.close();
+        System.out.println("\t switched to" + this.fireMode + "fire mode");
+    }
+    
+    void fire() {
+        switch(this.fireMode) {
+            case "single": {
+                super.fire();
+                break;
+            }
+            case "bursting": {
+                if (!isClipEmpty()) {
+                    this.ammo -= 3;
+                }
+                else {
+                    System.out.println("Reload Gun!!!");
+                }
+                break;
+            }
+            case "automatic": {
+                if (!isClipEmpty()) {
+                    while (!isClipEmpty()) {
+                        this.ammo--;
+                    }
+                }
+                else {
+                    System.out.println("Reload Gun!!!");
+                }
+                break;
+            }
+        }
     }
 }
