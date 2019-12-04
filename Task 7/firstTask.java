b92c3ebb4f648121324be3422e0c253b6d667d26
Veka Dzmitry <vekodmitriy0@gmail.com>
2019-12-04 14:41:05 +0300

    feat: implement Gun class

--- Task 7/firstTask.java
+++ Task 7/firstTask.java
@@ -7,3 +7,41 @@ public class firstTask {
         
     }
 }
+
+/**
+ * Gun
+ */
+class Gun {
+    int length;
+    int weight;
+    int clipCapacity;
+    String ammoType;
+    int ammo;
+
+    Gun() {
+
+    }
+    
+    Gun(int clipCapacity) {
+        this.clipCapacity = clipCapacity;
+        this.ammo = clipCapacity;
+    }
+
+    boolean isClipEmpty() {
+        if (this.ammo == 0) return true;
+        return false;
+    }
+
+    void reload() {
+        this.ammo = this.clipCapacity;
+    }
+
+    void fire() {
+        if (!isClipEmpty()) {
+            this.ammo--;
+        }
+        else {
+            System.out.println("Reload Gun!!!");
+        }
+    }
+}
